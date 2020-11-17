64bd3a53e860ca61a5853f9f88099748a0f78c99
Renan Martineli de Paula <renanmartineli@hotmail.com>
2020-11-17 16:13:05 -0300

    update build

--- sistema.js
+++ sistema.js
@@ -1,27 +1,17 @@
-// class Client {
-//   constructor(number, name, movie, rentStatus) {
-//     this.number = number;
-//     this.name = name;
-//     this.movie = movie;
-//     this.rentStatus = rentStatus;
-//   }
-// }
-
-// const table = {
-//   client: {
-//   },
-
-//   movie: {
-//   },
-// };
-let customTrIDPrefix = "tr";
-let customTrIDNumber = 0;
-
 const createTd = item => {
   const Td = document.createElement("td");
   Td.innerHTML = item;
   return Td;
 };
+
+const createTdWithI = item => {
+  const Td = document.createElement("td");
+  const i = document.createElement("i");
+  Td.innerHTML = item;
+  Td.setAttribute("class", "tdEdit");
+  Td.appendChild(i).setAttribute("class", "fas fa-edit");
+  return Td;
+}
 
 const appendChildren = (parent, children) => {
   children.forEach(child => {
@@ -29,8 +19,6 @@ const appendChildren = (parent, children) => {
     parent.appendChild(child);
   });
 };
-
-
 
 document.querySelector("#addClientBtn").addEventListener("click", () => {
   const clientName = document.querySelector("#name").value;
@@ -46,18 +34,20 @@ document.querySelector("#addClientBtn").addEventListener("click", () => {
   const appendTr = getTbody.appendChild(createTr);
 
   const items = [
-    createTd(localStorage.getItem("clientName")),
-    createTd(localStorage.getItem("clientMovie")),
-    createTd(localStorage.getItem("clientLocado")),
-    createTd('<i class="fas fa-edit"></i>'),
+    createTdWithI(localStorage.getItem("clientName")),
+    createTdWithI(localStorage.getItem("clientMovie")),
+    createTdWithI(localStorage.getItem("clientLocado")),
     createTd('<i class="fas fa-trash"></i>')
   ];
 
   appendChildren(appendTr, items);
   
   deleteRow();
+  updateItems();
 });
 
+
+// Deleta as linhas na tabela
 function deleteRow() {
   let trashIcon = document.querySelectorAll(".fa-trash");
   trashIcon[trashIcon.length - 1].addEventListener("click", event => {
@@ -65,6 +55,35 @@ function deleteRow() {
     trashIcon.parentNode.parentNode.parentNode.removeChild(trashIcon.parentNode.parentNode);
   });
 }
+
+function updateItems() {
+  let editIcon = document.querySelectorAll(".fa-edit");
+  let targetText = document.querySelectorAll(".tdEdit");
+  editIcon[targetText.length - 1].addEventListener("click", event => {
+    editIcon = event.target;
+    editIcon.innerText = "test";
+
+    // for (let i = 0; i < editIcon.length; i++) {
+    //   editIcon.length = i;
+    //   editIcon[i] = event.target;
+    //   editIcon[i].innerText = "testLocado";
+    // }
+
+
+    // if (editIcon.length === editIcon.length - 1) {
+    //   editIcon = event.target;
+    //   editIcon.innerText = "testLocado";
+    // } else if (editIcon.length === editIcon.length - 2) {
+    //   editIcon = event.target;
+    //   editIcon.parentNode.innerText = "testFilme";
+    // } else if (editIcon.length === editIcon.length - 3) {
+    //   editIcon = event.target;
+    //   editIcon.parentNode.innetText = "testNome";
+    // }
+      
+  });
+}
+
 
 // <i class="fas fa-edit"></i>
 // <i class="fas fa-trash"></i>
